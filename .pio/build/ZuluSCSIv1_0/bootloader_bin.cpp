f8aee9a6af7b88978c2e0cefb1557742d5495bee
Guy Taylor <geekon@gmail.com>
2025-09-03 12:41:07 +1000

    Commit to branch rebooting in msc

---
+++ .pio/build/ZuluSCSIv1_0/bootloader_bin.cpp
@@ -0,0 +1,6 @@
+
+__asm__(
+"   .section .text.btldr\n"
+"btldr:\n"
+"   .incbin \"Q:\\134Sweat\\134Code\\134RaspberryPiPico\\134MyChanges\\134ZuluSCSI-firmware\\134.pio\\134build\\134ZuluSCSIv1_0\\134bootloader.bin\"\n"
+);
